0e789fe1ad46d8c35ff8ad17afaa099af58cade9
Nhu-y Phan <nhucleus@gmail.com>
2021-02-24 18:38:17 -0500

    contact form updated

--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@
 		
 			// Set the recipient email address.
 	        // Type here your email id for receive email.
-	        $email_to = "wwfbcom@bootex.com"; 
+	        $email_to = "nhucleus@gmail.com"; 
 
 	        // Set the email subject.
 	        $subject = "New email from $name";
