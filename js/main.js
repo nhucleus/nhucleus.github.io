ebe11894efa15ee7cfb9df87f833c46092f6f958
Nhu-y Phan <nhucleus@gmail.com>
2021-03-12 10:52:40 -0500

    no pop up

--- js/main.js
+++ js/main.js
@@ -119,12 +119,12 @@ $(window).on('load', function() {
 	/*------------------
 		MAGNIDIC POPUP
 	--------------------*/
-	$('.work-item').magnificPopup({
-		type: 'image',
-		gallery: { enabled: true },
-		removalDelay: 400,
-		zoom:{enabled: true, duration: 300}
-	});
+	// $('.work-item').magnificPopup({
+	// 	type: 'image',
+	// 	gallery: { enabled: true },
+	// 	removalDelay: 400,
+	// 	zoom:{enabled: true, duration: 300}
+	// });
 
 
 	/*------------------
